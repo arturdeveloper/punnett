462fd09165032e608477f9e5a3d3cc8a0b1b864d
arturdeveloper <sato.alta@gmail.com>
2019-03-12 22:29:15 -0400

    Updates

---
+++ precache-manifest.dd454431ae1e6b03430eead2ac160859.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ab67e5b7238292c194aede557412928",
+    "url": "/punnett/static/media/yellow_pea.9ab67e5b.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/punnett/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "341f84d7701256987e7372345221c39c",
+    "url": "/punnett/static/media/green_pea.341f84d7.svg"
+  },
+  {
+    "revision": "f15412853b9bf329728f",
+    "url": "/punnett/static/js/runtime~main.ee3ce633.js"
+  },
+  {
+    "revision": "57ea1b3a493e36e54fbc",
+    "url": "/punnett/static/js/main.3b543fa5.chunk.js"
+  },
+  {
+    "revision": "66bc133348ac85841bb7",
+    "url": "/punnett/static/js/2.f489368d.chunk.js"
+  },
+  {
+    "revision": "57ea1b3a493e36e54fbc",
+    "url": "/punnett/static/css/main.9a753338.chunk.css"
+  },
+  {
+    "revision": "1bc1f585b5ce667c8a3be5599626dc3a",
+    "url": "/punnett/index.html"
+  }
+];
